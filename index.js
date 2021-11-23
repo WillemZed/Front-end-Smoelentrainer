1f73370f7e0985c7449baef9bd38997d43857b4c
Zed <willem.zheng.xu@gmail.com>
2021-11-23 22:07:41 +0100

    history list included, geen max 10, alerts werken niet meer

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const startTitle = document.getElementById("startTitle");
 const startButton = document.getElementById("startButton");
 const container = document.getElementById("container");
 
+const gear = document.getElementById("gear")
+const settings = document.getElementById("settings")
+
 const buttonContainer = document.getElementById("buttonContainer");
 const imageContainer = document.getElementById("imageContainer");
 
@@ -10,6 +13,12 @@ const resultContainer = document.getElementById("resultContainer");
 const correctPoints = document.getElementById("goeiePunten");
 const wrongPoints = document.getElementById("foutePunten");
 const matchPogingen = document.getElementById("matchPogingen")
+
+const showHistory = document.getElementById("showHistory")
+const goeieHistory = document.getElementById("goeieHistory");
+const fouteHistory = document.getElementById("fouteHistory")
+const history = document.getElementById("history")
+
 const button0 = document.getElementById("button0");
 const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
@@ -19,6 +28,11 @@ const img0 = document.getElementById("img0");
 const img1 = document.getElementById("img1");
 const img2 = document.getElementById("img2");
 
+const countdownBar = document.getElementById("countdownBar");
+const timeLeft = document.getElementById("timeLeft");
+
+
+var timerTime = 10;
 var dataId;
 var savedOptions = []
 
@@ -29,11 +43,64 @@ var correct = [];
 var wrong = [];
 var session = 3;
 
+var count = 0
 var rounds = 0;
 var matchAttemps = []
 
 var savedButton = []
 var savedImage = []
+
+window.onload = localBackgroundSetting();
+
+function localBackgroundSetting() {
+    const savedColor = localStorage.getItem("color")
+    var red = document.getElementById("red")
+    var green = document.getElementById("green")
+    var blue = document.getElementById("blue")
+
+    console.log(savedColor)
+    if (savedColor) {
+        if (savedColor == "red") {
+            document.body.style.backgroundColor = "red"
+            red.selected = true;
+        }
+        if (savedColor == "green") {
+            document.body.style.backgroundColor = "green"
+            green.selected = true;
+        }
+        if (savedColor == "blue") {
+            document.body.style.backgroundColor = "blue"
+            blue.selected = true;
+        }
+    }
+}
+
+function changeBackgroundColor() {
+    var backgroundColorSettings = document.getElementById("backgroundColor").value
+
+    localStorage.setItem("color", backgroundColorSettings)
+
+    const savedColor = localStorage.getItem("color")
+
+    if (backgroundColorSettings == savedColor) {
+        backgroundColorSettings.selected = "true"
+    }
+
+
+    if (savedColor) {
+        if (savedColor == "red") {
+            document.body.style.backgroundColor = "red"
+        }
+        if (savedColor == "green") {
+            document.body.style.backgroundColor = "green"
+        }
+        if (savedColor == "blue") {
+            document.body.style.backgroundColor = "lightblue"
+        }
+    }
+
+
+}
 
 
 
@@ -51,39 +118,75 @@ function hide(element) {
 
 hide(container);
 hide(resultContainer);
+hide(countdownBar)
+hide(history)
+hide(showHistory)
 
+showHistory.onclick = function () {
+    show(history);
+    showGoeieHistory()
 
+    
+    
+}
+gear.onclick = function () {
+    if (settings.classList == "hidden") {
+        show(settings)
+    } else {
+        hide(settings)
+    }
+}
 
-startButton.onclick = function() {
+startButton.onclick = function () {
+    correctAnswers = 0;
+    wrongAnswers = 0;
+    tries = 0;
+    hide(history)
+    hide(gear)
+    hide(settings)
+    hide(resultContainer)
     hide(allJojos);
     hide(startTitle);
     hide(startButton);
-    hide(resultContainer);
+    hide(showHistory)
     show(container);
     insertImage();
     insertName();
+    initBarCount()
+    deleteHistory()
 }
 
-skipBtn.onclick = function() {
+skipBtn.onclick = function () {
+    const btn = document.querySelector(".rememberBtnCol");
+    if (btn) {
+        btn.classList.remove("rememberBtnCol")
+    }
+    const img = document.querySelector(".rememberImgCol");
+
+    if (img) {
+        img.classList.remove("rememberImgCol")
+    }
+    savedButton.splice(0, 1)
+    savedImage.splice(0, 1)
     insertImage()
     insertName()
 }
 //functions when images are pressed
-img0.onclick = function() {
+img0.onclick = function () {
     dataId = this.dataset.id;
     colorImg()
     savedImage.push(dataId);
     buttonOnclick()
 }
 
-img1.onclick = function() {
+img1.onclick = function () {
     dataId = this.dataset.id;
     colorImg()
     savedImage.push(dataId);
     buttonOnclick()
 }
 
-img2.onclick = function() {
+img2.onclick = function () {
     dataId = this.dataset.id;
     colorImg()
     savedImage.push(dataId);
@@ -91,21 +194,21 @@ img2.onclick = function() {
 }
 
 //funcions when buttons are pressed
-button0.onclick = function() {
+button0.onclick = function () {
     dataId = this.dataset.id;
     colorBtn();
     savedButton.push(dataId);
     buttonOnclick()
 }
 
-button1.onclick = function() {
+button1.onclick = function () {
     dataId = this.dataset.id;
     colorBtn();
     savedButton.push(dataId);
     buttonOnclick()
 }
 
-button2.onclick = function() {
+button2.onclick = function () {
     dataId = this.dataset.id;
     colorBtn();
     savedButton.push(dataId);
@@ -114,7 +217,7 @@ button2.onclick = function() {
 
 //insert names in button tags
 function insertName() {
-    var nameArray =  [
+    var nameArray = [
         "Jonathan Joestar",
         "Joseph Joestar",
         "Jotaro Kujoh",
@@ -125,7 +228,8 @@ function insertName() {
         "Josuke higashikata(2)"
     ]
     var numsArray = [0, 1, 2, 3, 4, 5, 6, 7];
-    for(var i = 0; i <= 2; i++) {
+
+    for (var i = 0; i <= 2; i++) {
         const variableB = [button0, button1, button2];
 
         var randomNumber = Math.floor(Math.random() * numsArray.length);
@@ -158,7 +262,7 @@ function insertImage() {
 
     var numsArray = [0, 1, 2, 3, 4, 5, 6, 7];
 
-    for(var i = 0; i <= 2; i++) {
+    for (var i = 0; i <= 2; i++) {
         const variableI = [img0, img1, img2]
 
         var randomNumber = Math.floor(Math.random() * numsArray.length);
@@ -178,31 +282,30 @@ function insertImage() {
 }
 
 function buttonOnclick() {
-    if(savedImage[0] == savedImage[1]) {
+    if (savedImage[0] == savedImage[1]) {
         savedImage.splice(0, 2);
-    } 
-    if(savedButton[0] == savedButton[1]) {
+    }
+    if (savedButton[0] == savedButton[1]) {
         savedButton.splice(0, 2);
     } else {
-        if(savedImage.length >=2) {
+        if (savedImage.length >= 2) {
             const poppedI = savedImage.pop();
             savedImage[0] = poppedI;
         }
 
-        if(savedButton.length >=2) {
+        if (savedButton.length >= 2) {
             const poppedB = savedButton.pop();
             savedButton[0] = poppedB;
         }
 
-        if (savedImage.length !=0 && savedButton.length !=0) {
+        if (savedImage.length != 0 && savedButton.length != 0) {
             comparison();
         }
     }
 
-} 
+}
 
-function comparison() 
-{   
+function comparison() {
 
     if (savedImage[0] == savedButton[0]) {
         correctFunction();
@@ -211,30 +314,35 @@ function comparison()
     }
 
     if (tries != session) {
+        count++
         insertImage()
         insertName()
-        console.log("tries = " + tries +" and session = " + session)
+        console.log("tries = " + tries + " and session = " + session)
         console.log(correctAnswers);
         console.log(wrongAnswers)
     } else {
+        count++
         correct.push(correctAnswers);;
         wrong.push(wrongAnswers);
         rounds++
         matchAttemps.push(rounds);
+        if (matchAttemps == undefined) {
+            matchAttemps.push(0)
+        }
         hide(container);
+        hide(countdownBar)
         show(startButton);
         startButton.value = "Herstart de trainer";
         show(resultContainer);
+        show(showHistory)
         showResults();
         console.log(correctAnswers);
         console.log(wrongAnswers)
-        correctAnswers = 0;
-        wrongAnswers = 0;
-        tries = 0;
     }
 }
 
 function correctFunction() {
+    alert("goed :D")
     const btn = document.querySelector(".rememberBtnCol");
     btn.classList.remove("rememberBtnCol")
 
@@ -247,6 +355,7 @@ function correctFunction() {
 }
 
 function wrongFunction() {
+    alert("WRONG")
     const btn = document.querySelector(".rememberBtnCol");
     btn.classList.remove("rememberBtnCol")
 
@@ -258,45 +367,119 @@ function wrongFunction() {
     tries++
     wrongAnswers++
 }
-function colorBtn(){
+
+function colorBtn() {
     const id = document.querySelector("button[data-id='" + dataId + "']");
-    if(id.classList.contains("rememberBtnCol")) {
+    if (id.classList.contains("rememberBtnCol")) {
         id.classList.remove("rememberBtnCol");
     } else {
-        if(id){
+        if (id) {
             button0.classList.remove("rememberBtnCol");
             button1.classList.remove("rememberBtnCol");
             button2.classList.remove("rememberBtnCol");
             id.classList.add("rememberBtnCol")
-        }else{
+        } else {
             button0.classList.remove("rememberBtnCol");
             button1.classList.remove("rememberBtnCol");
             button2.classList.remove("rememberBtnCol");
         }
     }
-    
+
 }
 
 function colorImg() {
     const id = document.querySelector("img[data-id='" + dataId + "']");
-    if(id.classList.contains("rememberImgCol")) {
+    if (id.classList.contains("rememberImgCol")) {
         id.classList.remove("rememberImgCol")
     } else {
-        if(id){
+        if (id) {
             img0.classList.remove("rememberImgCol");
             img1.classList.remove("rememberImgCol");
             img2.classList.remove("rememberImgCol");
             id.classList.add("rememberImgCol")
-        }else{
+        } else {
             img0.classList.remove("rememberImgCol");
             img1.classList.remove("rememberImgCol");
             img2.classList.remove("rememberImgCol");
         }
     }
+}
+
+function initBarCount() {
+    show(countdownBar)
+    var startTimer = setInterval(barCount, timerTime)
+    console.log(startTimer);
+
+    function barCount() {
+
+        if (timeLeft.clientWidth == countdownBar.clientWidth + "px") {
+            countdownBar.width = timeLeft.clientWidth + "px";
+        }
+        if (timeLeft.clientWidth < countdownBar.clientWidth) {
+            timeLeft.style.width = timeLeft.clientWidth + 1 + "px";
+        } else {
+            timeLeft.style.width = 0;
+            countdownBar.style.width = 500 + "px";
+            if (timeLeft.style.width == countdownBar.style.width + "px") {
+                console.log(timeLeft.style.width)
+                console.log(countdownBar.style.width)
+                console.log("width is hetzeflde")
+            }
+            clearInterval(startTimer)
+            skipToEnd();
+            hide(countdownBar)
+        }
+    }
+}
+
+function skipToEnd() {
+    if(rounds == 0) {
+        rounds++
+        matchAttemps.push(rounds);
+        rounds = 0;
+        correct.push(correctAnswers);;
+        wrong.push(wrongAnswers);
+    }
+    // rounds++
+    // matchAttemps.push(rounds);
+    hide(container);
+    show(startButton);
+    startButton.value = "Herstart de trainer";
+    show(resultContainer);
+    showResults();
 }
 
 function showResults() {
     correctPoints.innerHTML = "aantal goeie punten: " + correct[correct.length - 1]
     wrongPoints.innerHTML = "aantal verkeerde punten: " + wrong[wrong.length - 1];
     matchPogingen.innerHTML = "aantal matchpogingen: " + matchAttemps[matchAttemps.length - 1];
+}
+
+
+function showGoeieHistory() {
+    for(var i = 0; i <= matchAttemps.length - 1; i++) {
+        console.log(`loop = ${i}`)
+        var divG = document.createElement("div");
+        divG.id = "goeieHistory" + i;
+        divG.innerHTML = `aantal correct: ${correct[i]}`
+
+        var divW = document.createElement("div");
+        divW.id = "wrongHistory" + i;
+        divW.innerHTML = `aantal fout: ${wrong[i]}`
+
+        var divT = document.createElement("div");
+        divT.id = "match" + i;
+        divT.innerHTML = `Match: ${matchAttemps[i]}`
+
+        history.appendChild(divT)
+        history.appendChild(divG)
+        history.appendChild(divW)
+        
+    }
+}
+
+function deleteHistory() {
+    while(history.firstChild) {
+        history.removeChild(history.firstChild)
+    }
 }
